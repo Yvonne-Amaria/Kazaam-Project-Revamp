5c0e7e20f6e2f42da5e9939fd933f60cdfe13ed5
Benjamin Woods <benewoods02@gmail.com>
2022-07-27 19:08:19 +0000

    added a working ShazamAPI test

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from ShazamAPI import Shazam
+
+#Test
+mp3_file_content_to_recognize = open('Time Alone.mp3', 'rb').read()
+
+shazam = Shazam(mp3_file_content_to_recognize)
+recognize_generator = shazam.recognizeSong()
+print(next(recognize_generator))  # current offset & shazam response to recognize requests
 
